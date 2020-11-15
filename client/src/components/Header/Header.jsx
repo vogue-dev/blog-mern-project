eb78458328380c1563ab15450728b59a355b4cf8
Vogue <oprya1111@yandex.ru>
2020-11-15 03:06:57 +0200

    test prettier tabWidth

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,29 +5,29 @@ import { headerLinks } from '../../utils/constants.js';
 import './header.scss';
 
 const Header = () => {
-	return (
-		<header>
-			<div className="container">
-				<div className="row">
-					<div className="logo">
-						<Link to="/">Logo</Link>
-					</div>
-					<div className="links">
-						<ul>
-							{headerLinks.map(({ link, value }, i) => (
-								<Link to={link} key={i}>
-									<li>{value}</li>
-								</Link>
-							))}
-						</ul>
-					</div>
-					<div className="login">
-						<Link to="/admin">Админка</Link>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                    <div className="logo">
+                        <Link to="/">Logo</Link>
+                    </div>
+                    <div className="links">
+                        <ul>
+                            {headerLinks.map(({ link, value }, i) => (
+                                <Link to={link} key={i}>
+                                    <li>{value}</li>
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="login">
+                        <Link to="/admin">Админка</Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
--- server/models/postsUA.js
+++ server/models/postsUA.js
@@ -1,31 +1,31 @@
 import mongoose from 'mongoose';
 
 const postsUASchema = mongoose.Schema({
-	title: String,
-	titleStatus: {
-		tyle: String,
-		default: '',
-	},
-	price: String,
-	description: String,
-	date: String,
-	readingTime: {
-		type: String,
-		default: '2',
-	},
-	image: String,
-	creator: {
-		type: String,
-		default: 'admin',
-	},
-	likeCount: {
-		type: Number,
-		default: 0,
-	},
-	createdAt: {
-		type: Date,
-		default: new Date(),
-	},
+    title: String,
+    titleStatus: {
+        tyle: String,
+        default: '',
+    },
+    price: String,
+    description: String,
+    date: String,
+    readingTime: {
+        type: String,
+        default: '2',
+    },
+    image: String,
+    creator: {
+        type: String,
+        default: 'admin',
+    },
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
 });
 
 const PostsUA = mongoose.model('ua-posts', postsUASchema);
