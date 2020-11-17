537c56c80d2bba51d9a346dade0f578fd4b3816a
Vogue <oprya1111@yandex.ru>
2020-11-17 22:23:13 +0200

    add footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,10 +6,36 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer container">
-                <div className="row">
-                    <div className="first-col"></div>
-                    <div className="second-col"></div>
-                    <div className="third-col"></div>
+                <div className="footer__content">
+                    <div className="footer__content--left-section">Logo</div>
+
+                    <div className="footer__content--right-section">
+                        <div className="first-col">
+                            <ul>
+                                Product
+                                <li>Log In</li>
+                                <li>Sign In</li>
+                                <li>How it works</li>
+                                <li>Features</li>
+                                <li>Testimonials</li>
+                                <li>Pricing</li>
+                            </ul>
+                        </div>
+                        <div className="second-col">
+                            <ul>
+                                Support
+                                <li>Help Center</li>
+                                <li>Privacy Policy</li>
+                                <li>Terms of Service</li>
+                                <li>Cookie Policy</li>
+                                <li>Do Not Sell My Info</li>
+                                <li>Accessibility</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer__bottom">
+                    <div className="footer__bottom--content">© 2020 Created by vogue-dev</div>
                 </div>
             </div>
         </footer>
